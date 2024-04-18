009e69f74605b8440f43e61b4afec95a06967a58
KhanhSun <2224802010480@student.tdmu.edu.vn>
2024-04-18 17:41:59 +0700

    Rebuild Construct
    tái cấu trúc lại file

---
+++ QuanLyKhachSanATD/CacChucNang/DichVu/FormDichVu.Designer.cs
@@ -0,0 +1,84 @@
+﻿namespace QuanLyKhachSan.DichVu
+{
+    partial class FormDichVu
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormDichVu));
+            listView1 = new ListView();
+            pictureBox1 = new PictureBox();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            SuspendLayout();
+            // 
+            // listView1
+            // 
+            listView1.Location = new Point(24, 279);
+            listView1.Name = "listView1";
+            listView1.Size = new Size(200, 235);
+            listView1.TabIndex = 0;
+            listView1.UseCompatibleStateImageBehavior = false;
+            listView1.SelectedIndexChanged += listView1_SelectedIndexChanged;
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.BackColor = Color.White;
+            pictureBox1.BackgroundImageLayout = ImageLayout.None;
+            pictureBox1.Image = QuanLyKhachSanATD.Properties.Resources.hoyoverse_logo_42473;
+            pictureBox1.Location = new Point(81, 64);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(424, 332);
+            pictureBox1.TabIndex = 1;
+            pictureBox1.TabStop = false;
+            pictureBox1.Click += pictureBox1_Click;
+            // 
+            // FormDichVu
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = SystemColors.ActiveCaption;
+            BackgroundImage = (Image)resources.GetObject("$this.BackgroundImage");
+            ClientSize = new Size(817, 526);
+            ControlBox = false;
+            Controls.Add(pictureBox1);
+            Controls.Add(listView1);
+            ForeColor = Color.Snow;
+            Icon = (Icon)resources.GetObject("$this.Icon");
+            MaximizeBox = false;
+            MinimizeBox = false;
+            Name = "FormDichVu";
+            Text = "FormDichVu";
+            Load += FormDichVu_Load;
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private ListView listView1;
+        private PictureBox pictureBox1;
+    }
+}
